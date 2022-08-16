53fcccdbe5184e1d882d3c951e67d345e50a07e6
Rafael Souza <souza_rafael.97@outlook.com>
2022-08-16 19:52:09 -0300

    Adiciona teste do requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente "<About.js />"', () => {
+  test('Teste se é exibido uma mensagem caso não tenha pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavoritesMessage = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavoritesMessage).toBeInTheDocument();
+  });
+});
