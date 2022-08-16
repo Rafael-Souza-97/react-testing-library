5a6ed6b5f446f4c47ae9c493df9d8fd9d6928efc
Rafael Souza <souza_rafael.97@outlook.com>
2022-08-16 20:01:18 -0300

    Adiciona testes do requisito 4 e corrige requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -3,7 +3,7 @@ import { screen } from '@testing-library/react';
 import FavoritePokemons from '../pages/FavoritePokemons';
 import renderWithRouter from './renderWithRouter';
 
-describe('Teste o componente "<About.js />"', () => {
+describe('Teste o componente "<FavoritePokemons.js />"', () => {
   test('Teste se é exibido uma mensagem caso não tenha pokémons favoritos', () => {
     renderWithRouter(<FavoritePokemons />);
     const noFavoritesMessage = screen.getByText(/No favorite pokemon found/i);
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente "<NotFound.js />"', () => {
+  test('Teste se a página contém um heading "h2" com "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundMessage = screen.getByRole('heading', { level: 2, name: /not found/i });
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+
+  test('Teste se a página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundImage = screen.getByRole('img', { name: /Pikachu crying/i });
+    expect(notFoundImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
